36b2cf567f58580ddd57306cc259fa88d68a95f3
jordi <a18jorgornei@inspedralbes.cat>
2021-04-09 01:23:44 +0200

    security, findbyrole

--- src/Controller/CapRoleController.php
+++ src/Controller/CapRoleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CapRoleController extends AbstractController
 {
     /**
-     * @Route("/cap/role", name="cap_role")
+     * @Route("/caprole", name="cap_role")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_CAP', null, 'User tried to access a page without having ROLE_ADMIN');
+
         return $this->render('cap_role/index.html.twig', [
-            'controller_name' => 'CapRoleController',
+            'caps' => $userRepository->findByRole('ROLE_CAP'),
+            'users' => $userRepository->findByRole('ROLE_USER'),
         ]);
     }
 }
--- src/Controller/UserRoleController.php
+++ src/Controller/UserRoleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserRoleController extends AbstractController
 {
     /**
-     * @Route("/user/role", name="user_role")
+     * @Route("/userrole", name="user_role")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'User tried to access a page without having ROLE_ADMIN');
+
         return $this->render('user_role/index.html.twig', [
-            'controller_name' => 'UserRoleController',
+            'users' => $userRepository->findByRole('ROLE_USER'),
         ]);
     }
 }
